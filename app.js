5fa8c0469e1b8e062366c5fa5557f882e4dd638e
graygabrielle <gray.atkinson.adams@gmail.com>
2019-04-24 16:31:24 -0700

    set up more routes, get and post requests, server side form validation

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const app = express();
 //map global promise-get rid of warning
 // mongoose.Promise = global.Promise;
 
+//body parser middleware
+app.use(express.urlencoded({ extended: false}));
+app.use(express.json());
+
 //Connect to mongoose
 mongoose.connect("mongodb://localhost/ideaboard-dev", {
   useNewUrlParser: true
@@ -27,12 +31,35 @@ app.get(`/`, (req, res) => {
 //about route
 app.get(`/about`, (req, res) => {
   res.render('about');
-})
+});
 
 //add Idea form
 app.get(`/ideas/add`, (req, res) => {
   res.render('ideas/add');
-})
+});
+
+//process form
+app.post(`/ideas`, (req, res) =>{
+  let errors = [];
+
+  if(!req.body.title){
+    errors.push({text:'Please add a title'});
+  }
+  if(!req.body.details){
+    errors.push({text:'Please add some details'});
+  }
+
+  if(errors.length>0){
+    res.render('ideas/add', {
+      errors: errors,
+      title: req.body.title,
+      details: req.body.details
+    });
+  } else{
+    res.send('passed');
+  }
+
+});
 
 //handlebars middleware
 app.engine('handlebars', handlebars({defaultLayout: 'main'}));
@@ -40,7 +67,7 @@ app.set('view engine', 'handlebars');
 
 
 //set up port and run server
-const port = 5000;
+const port = process.env.PORT || 5000;
 app.listen(port, () =>{
   console.log(`Server started on port http://localhost:${port}`);
-})
+});
