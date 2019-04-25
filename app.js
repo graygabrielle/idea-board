27d29eee86182599f0364e9512d776ae578b8985
graygabrielle <gray.atkinson.adams@gmail.com>
2019-04-25 00:08:15 -0700

    added logout capability

--- app.js
+++ app.js
@@ -23,6 +23,10 @@ app.use(session({
   saveUninitialized: true
 }));
 
+//passport middleware (MUST BE AFTER EXPRESS SESSION MIDDLEWARE!!)
+app.use(passport.initialize());
+app.use(passport.session());
+
 //Flash middleware
 app.use(flash());
 
@@ -31,6 +35,7 @@ app.use(function(req, res, next){
   res.locals.success_msg = req.flash('success_msg');
   res.locals.error_msg = req.flash('error_msg');
   res.locals.error = req.flash('error');
+  res.locals.user = req.user || null;
   next();
 });
 
